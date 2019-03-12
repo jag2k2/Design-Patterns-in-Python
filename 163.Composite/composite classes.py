9e8dbce120ff1edc5ed42deddb31c63b1d43a94a
jag2k2 <jag2k2@gmail.com>
2019-03-12 15:15:16 -0500

    Initial commit

---
+++ 163.Composite/composite classes.py
@@ -0,0 +1,97 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Equipment(ABC):
+
+    def __init__(self, name: str)->None:
+        self._name = name
+
+    def add(self, component: "Equipment")->None:
+        raise Exception("Cannot add components to leaves")
+
+    def remove(self, component: "Equipment")->None:
+        raise Exception("Cannot remove components from leaves")
+
+    def name(self)->str:
+        return self._name
+
+    @abstractmethod
+    def power(self)->int:
+        pass
+
+    @abstractmethod
+    def net_price(self)->int:
+        pass
+
+
+class CompositeEquipment(Equipment):
+
+    def __init__(self, name: str)->None:
+        super().__init__(name)
+        self._equip_list: List[Equipment] = []
+
+    def add(self, component: "Equipment")->None:
+        self._equip_list.append(component)
+
+    def remove(self, component: "Equipment")->None:
+        self._equip_list.remove(component)
+
+    def power(self)->int:
+        power_sum: int = 0
+        for i in self._equip_list:
+            power_sum += i.power()
+        return power_sum
+
+    def net_price(self)->int:
+        net_sum: int = 0
+        for i in self._equip_list:
+            net_sum += i.net_price()
+        return net_sum
+
+
+class Floppy(Equipment):
+    _power = 15
+    _net_price = 10
+
+    def power(self)->int:
+        return self._power
+
+    def net_price(self)->int:
+        return self._net_price
+
+
+comp1 = Floppy("floppy1")
+comp2 = Floppy("floppy2")
+comp3 = Floppy("floppy3")
+comp4 = Floppy("floppy4")
+comp5 = Floppy("floppy5")
+group1 = CompositeEquipment("composite1")
+group2 = CompositeEquipment("composite2")
+
+group1.add(comp1)
+group1.add(comp2)
+group1.add(group2)
+group2.add(comp3)
+group2.add(comp4)
+group2.add(comp5)
+
+print(comp1.net_price())
+print(comp4.net_price())
+print(group2.net_price())
+print(group1.net_price())
+print("")
+
+try:
+    comp1.add(comp2)
+except Exception as err:
+    print(err)
+group2.remove(comp3)
+
+print(comp1.net_price())
+print(comp4.net_price())
+print(group2.net_price())
+print(group1.net_price())
+
+
+
