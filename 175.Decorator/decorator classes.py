bdfd53303afa2ee82071f7b5304e736781b720f0
jag2k2 <jag2k2@gmail.com>
2019-04-19 14:55:22 -0500

    simplified the example and added proper spacing to each "->"

--- 175.Decorator/decorator classes.py
+++ 175.Decorator/decorator classes.py
@@ -4,21 +4,14 @@ from abc import ABC, abstractmethod
 class VisualComponent(ABC):
 
     @abstractmethod
-    def draw(self)->None:
-        pass
-
-    @abstractmethod
-    def resize(self)->None:
+    def draw(self) -> None:
         pass
 
 
 class TextView(VisualComponent):
 
-    def draw(self)->None:
+    def draw(self) -> None:
         print("Drawing contents of TextView")
-
-    def resize(self)->None:
-        print("Resizing contents of TextView")
 
 
 class Decorator(VisualComponent):
@@ -26,32 +19,32 @@ class Decorator(VisualComponent):
     def __init__(self, component: VisualComponent):
         self._component: VisualComponent = component    # enclose object into the decorator
 
-    def draw(self)->None:
+    @abstractmethod
+    def draw(self) -> None:
         self._component.draw()                          # forward request to the enclosed object
-
-    def resize(self)->None:
-        self._component.resize()                        # forward request to the enclosed object
 
 
 class BorderDecorator(Decorator):
 
-    def __init__(self, component: VisualComponent, border_width: int)->None:
-        self._border_width: int = border_width
+    def __init__(self, component: VisualComponent) -> None:
+        self._border_width = 0
         super().__init__(component)
 
-    def draw_border(self)->None:
-        print("Drawing border with width " + str(self._border_width))
+    def set_border(self, border_width: int) -> None:
+        self._border_width = border_width
 
-    def draw(self)->None:
-        super().draw()
-        self.draw_border()
+    def draw(self) -> None:
+        print("Doing something first.")
+        super().draw()                    # call the wrapped function
+        print("Do something after: Drawing border with width " + str(self._border_width))
 
 
 glyph: VisualComponent = TextView()
 glyph.draw()
-glyph.resize()
+print("")
 
-border_dec: BorderDecorator = BorderDecorator(glyph, 3)   # wrap glyph into border_dec
+border_dec: BorderDecorator = BorderDecorator(glyph)   # wrap glyph into border_dec
+border_dec.set_border(3)
 border_dec.draw()       # border_dec has the same interface as glyph but modifies that object's capabilities
 
 
